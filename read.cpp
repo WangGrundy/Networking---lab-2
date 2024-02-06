9d8c2f75c5fc4b8f0cb4458a5091bec1e6209412
JDgoldvox <VoxcastJDgoldvox@gmail.com>
2024-02-06 10:18:39 -0500

    fixed last Q

--- read.cpp
+++ read.cpp
@@ -10,88 +10,26 @@ union IntConverter
     unsigned char as_bytes[sizeof(int)];
 };
 
+struct EntityInfo
+{
+    float x;
+    float y;
+    float z;
+    char name[40];
+    int health;
+};
+
 void read_game_entity(std::istream& file, float& x, float& y, float& z, std::string& name, int& health)
 {
-    using namespace std;
-    std::ifstream* file_as_ifstream = (std::ifstream*)(&file);
+    EntityInfo entity;
+    file.read((char*)(&entity), sizeof(EntityInfo));
 
-    //x, y, z, (floats)
-    //name, (up to 40 bytes)
-    //health, (int)
+    x = entity.x;
+    y = entity.y;
+    z = entity.z;
+    name = entity.name;
+    health = entity.health;
 
-    cout << "float amount of bytes: " << sizeof(float) << endl;
-    cout << "int amount of bytes: " << sizeof(int) << endl;
-
-    // the first 12 bytes split as [4][4][4] are all floats
-    //read 12 bytes
-    char coords[12];
-    float floatCoords[3];
-    read_n(*file_as_ifstream, coords, 12);
-
-    IntConverter converter;
-    convert_to_ints(coords, (int*)floatCoords, 12);
-
-    x = floatCoords[0];
-    y = floatCoords[1];
-    z = floatCoords[2];
-
-    //read the next bytes until delimiter
-    name = read_null_terminated_string(*file_as_ifstream);
-
-    //////////////////////////
-    /////////////////////////
-    ////////////////////////////
-    /////////////////////////
-    
-    //I think the terminated string stole the last 4 bytes.
-    /*char last4Bytes[4];
-
-    last4Bytes[0] = name[name.size() - 3];
-    last4Bytes[1] = name[name.size() - 2];
-    last4Bytes[2] = name[name.size() - 1];
-    last4Bytes[3] = name[name.size() - 0];
-
-    int calculatedHealth;
-    convert_to_ints(last4Bytes, &calculatedHealth, 4);
-
-    cout << "health: " << calculatedHealth << endl;*/
-
-    //////////////////////////////////////////////////////////////
-    file.clear(); // clear fail and eof bits
-    file.seekg(0, std::ios::beg); // back to the start!
-    //first 3 floats = 12, string = 40, last 4 is int... = 56
-    char entireFile[56];
-    file.read(entireFile, 56);
-    
-    int calculatedHealth;
-    char last4bytes[4] = { entireFile[0], entireFile[1] , entireFile[2] , entireFile[3] };
-    convert_to_ints(last4bytes, &calculatedHealth, 4);
-    health = calculatedHealth;
-
-    //CANNOT READ HEALTH. WON'T RESET FILE READ POSITION. HEALTH SHOULD BE THE FIRST 4 BYTES BUT IS
-    //CLEARLY NOT.
-
-    ////////////////////////////////////////////
-
-    //find the last remaining bytes and convert to int, its the health
-    //this shit reads 4 bytes
-    //last is [4]
-
-    //char healthBytes[4];
-    //file.read(healthBytes, 4);
-    ////read_n(*file_as_ifstream, healthBytes, 4);
-
-    ///*for each(auto byte in healthBytes) {
-    //    cout << byte << endl;
-    //}*/
-
-
-    //int calculatedHealth;
-    //convert_to_ints(healthBytes, &calculatedHealth, 4);
-
-    //cout << calculatedHealth << endl;
-
-    cout << endl << endl << endl << endl << endl << endl;
     return;
 }
 
